806424f8c1a588457f0052159ac489fe300e0371
Kidchai <o.naibova@gmail.com>
2023-08-09 08:43:55 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,26 @@
+## Simple console app imitating a bank account
+
+### Task description
+
+You are developing a simple banking application. Create a Java class called "BankAccount" to represent a user's bank account. The class should have private fields for the account number, account holder's name, balance, and a static variable for the bank's interest rate (set to 2.5%). Implement methods to deposit money, withdraw money, check the balance, and calculate the interest earned for a given period.
+
+### How to run?
+
+**1. Clone the repository and navigate to the project directory**
+
+```bash
+$ git clone https://github.com/Kidchai/banking-account.git
+$ cd library-management-system/src
+```
+
+**2. Compile the application**
+
+```bash
+$ javac BankAccount.java Main.java
+```
+
+**3. Run the application**
+
+```bash
+$ java Main
+```
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = createAccount();
+
+        while (true) {
+            displayMenu();
+            getOperation(account);
+        }
+    }
+
+    private static BankAccount createAccount() {
+        return new BankAccount("123456789", "Anna Karenina", 1000);
+    }
+
+    private static void displayMenu() {
+        System.out.println("""
+            
+            Please enter the number of operations you want to perform:
+            1. Check balance
+            2. Deposit
+            3. Withdraw
+            4. Calculate interest
+            5. Exit""");
+    }
+
+    private static void getOperation(BankAccount account) {
+        int operation = scanner.nextInt();
+        switch (operation) {
+            case 1 -> checkBalance(account);
+            case 2 -> deposit(account);
+            case 3 -> withdraw(account);
+            case 4 -> calculateInterest(account);
+            case 5 -> System.exit(0);
+            default -> System.out.println("Invalid operation. Please try again.");
+        }
+    }
+
+    private static void checkBalance(BankAccount account) {
+        System.out.printf("Your balance is: %d\n", account.getBalance());
+    }
+
+    private static void deposit(BankAccount account) {
+        System.out.println("Please enter the amount you want to deposit:");
+        int amount = scanner.nextInt();
+        account.setBalance(account.getBalance() + amount);
+        System.out.printf("Your balance is: %d\n", account.getBalance());
+    }
+
+    private static void withdraw(BankAccount account) {
+        System.out.println("Please enter the amount you want to withdraw:");
+        int amount = scanner.nextInt();
+        if (amount > account.getBalance()) {
+            System.out.println("You don't have enough money in your account.");
+        } else {
+            account.setBalance(account.getBalance() - amount);
+            System.out.printf("Your balance is: %d\n", account.getBalance());
+        }
+    }
+
+    private static void calculateInterest(BankAccount account) {
+        System.out.println("Please enter the period in months:");
+        double periodInMonths = Double.parseDouble(scanner.next());
+        double interest = account.getBalance() * (account.getInterestRate() / 100) * (periodInMonths / 12);
+        System.out.printf("Your interest is: %.2f\n", interest);
     }
 }
